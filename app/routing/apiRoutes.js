2f50c78118abbb3ece3934076ec0c8455df85f67
Jake Thornberry <wjthornberry@gmail.com>
2017-08-13 16:59:29 -0500

    Add GET route to display JSON of all possible friends and a POST route to handle incoming survey results -- also handles the compatability logic

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,53 @@
+var friend = require('../data/friends.js');
+
+module.exports = function(app) {
+    // API GET requests
+    app.get('/api/friends', function (req, res) {
+        res.json(friends);
+    });
+
+    // Takes the user's submission of a form and submits
+    // that data to the server
+    app.post('api/friends', function (req, res) {
+       
+        var friendMatch = {
+            name: '',
+            photo: '',
+            friendDifference: 1000
+        };
+        // Takes the result of the user's survey POST and parses it
+        var userData = req.body;
+        var userScores = userData.scores;
+
+        // Calculates difference between user's score with other user's scores in the db
+        var totalDifference = 0;
+
+        // Loop through all of the potential friends in the db
+        for (var i = 0; i < friends.length; i++) {
+            console.log(friends[i].name);
+            totalDifference = 0;
+
+            // Loops through each friend's scores
+            for (var j = 0; j < friends[i].scores[j]; j++) {
+
+                // Calculates the difference between the scores
+                // Adds them to totalDifference
+                totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
+
+                // Checks the sum of the differences
+                // If less than the differences of the current best match
+                if (totalDifference <= friendMatch.friendDifference) {
+                    // Reset the friendMatch to be the new friend
+                    friendMatch.name = friends[i].name;
+                    friendMatch.photo = friends[i].photo;
+                    friendMatch.friendDifference = totalDifference;
+                }
+            }
+        }
+        // Saves the user's data to the db
+        friends.push(userData);
+
+        // Returns a JSON with user's friendMatch. 
+        res.json(friendMatch);
+    });
+};
