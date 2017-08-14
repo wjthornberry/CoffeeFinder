d00b3e8151c8a02d5533adcdf86a820cb0a1f453
Jake Thornberry <wjthornberry@gmail.com>
2017-08-13 21:27:32 -0500

    Populate array with friends

---
+++ app/data/friends.js
@@ -0,0 +1,181 @@
+var friends = [
+    {
+        name: 'Aslan',
+        photo: '.././images/aslan.jpg',
+        scores: [
+            '5',
+            '5',
+            '5',
+            '1',
+            '1',
+            '5',
+            '3',
+            '2',
+            '3',
+            '2'
+        ]
+    },
+    {
+        name: 'Kathryn',
+        photo: '.././images/kathryn.jpg',
+        scores: [
+            '5',
+            '4',
+            '5',
+            '1',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5'
+        ]
+    },
+    {
+        name: 'Jake',
+        photo: '.././images/jake.png',
+        scores: [
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5'
+        ]
+    },
+    {
+        name: 'GOB',
+        photo: '.././images/gob.png',
+        score: [
+            '1',
+            '2',
+            '2',
+            '1',
+            '3',
+            '4',
+            '5',
+            '2',
+            '1',
+            '2'
+        ]
+    },
+    {
+        name: 'Donald Trump',
+        photo: '.././images/thedonald.png',
+        score: [
+            '1',
+            '4',
+            '1',
+            '1',
+            '1',
+            '2',
+            '5',
+            '1',
+            '1',
+            '4'
+        ]
+    },
+    {
+        name: 'Barack Obama',
+        photo: '.././images/barack.png',
+        score: [
+            '5',
+            '4',
+            '4',
+            '1',
+            '3',
+            '5',
+            '3',
+            '3',
+            '4',
+            '3',
+        ]
+    },
+    {
+        name: 'Harriet',
+        photo: '.././images/harriet.png',
+        score: [
+            '4',
+            '3',
+            '5',
+            '5',
+            '5',
+            '3',
+            '4',
+            '1',
+            '4',
+            '2'
+        ]
+    },
+    {
+        name: 'Paul McCartney',
+        photo: '.././images/paul.png',
+        score: [
+            '4',
+            '5',
+            '3',
+            '2',
+            '3',
+            '4',
+            '4',
+            '5',
+            '3',
+            '3'
+        ]
+    },
+    {
+       name: 'Ruth Bader Ginsburg',
+       photo: '.././images/ruth.png',
+       score: [
+           '4',
+           '3',
+           '4',
+           '1',
+           '2',
+           '5',
+           '2',
+           '2',
+           '3',
+           '3'
+       ] 
+    },
+    {
+        name: 'Calvin',
+        photo: '.././images/calvin.png',
+        score: [
+            '5',
+            '4',
+            '5',
+            '2',
+            '1',
+            '3',
+            '3',
+            '5',
+            '1',
+            '5'
+        ]
+    },
+    {
+        name: 'Hobbes',
+        photo: '.././images/hobbes.png',
+        score: [
+            '5',
+            '2',
+            '5',
+            '2',
+            '4',
+            '4',
+            '5',
+            '4',
+            '2',
+            '5'
+        ]
+    }
+];
+
+// Exports friends array
+module.exports = friends;
