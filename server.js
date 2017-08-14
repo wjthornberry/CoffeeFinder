2e515e1279339d12ea3b0caf1b4ea918aa6ecf8f
Jake Thornberry <wjthornberry@gmail.com>
2017-08-14 13:55:14 -0500

    Typos. Version working

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 // Dependencies - npm packages to give the server funcitonality
-var app = require('express')(),
+var express = require('express'),
     bodyParser = require('body-parser'),
     path = require('path');
 
 // Express configuration to set up the basic properties of the server
+var app = express();
 
 // Sets the server port.
 var PORT = process.env.PORT || 3000;
 
 app.listen(PORT, function () {
-    console.log(`Listening on Port ${PORT}`')
+    console.log(`Listening on Port ${PORT}`)
 });
 
 // Sets up bodyParser, which allows the server to interpret the data sent to it
