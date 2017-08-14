caf0410e7d64797cde7f27b8b9e22d8a2abcd128
Jake Thornberry <wjthornberry@gmail.com>
2017-08-14 15:01:09 -0500

    Change single quotes to double quotes for GET requests

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -6,11 +6,15 @@ var path = require('path');
 // HTML GET Requests - the below code handles when a user visits a page
 // In this case, HTML content will be displayed to them - either the survey page or the home page
 module.exports = function (app) {
+
+    app.get("/", function (req, res) {
+        res.sendFile(path.join(__dirname, "/../public/home.html"))
+    })
     app.get('/survey', function (req, res) {
-        res.sendFile(path.join(__dirname + '/../public/survey.html'));
+        res.sendFile(path.join(__dirname + "/../public/survey.html"));
     });
     // If there isn't a matching route, the default route sends the user to the home page
-    app.use(function (req, res) {
-        res.sendFile(path.join(__dirname + '/../public/home.html'));
-    });
+    // app.use(function (req, res) {
+    //     res.sendFile(path.join(__dirname + '/../public/home.html'));
+    // });
 };
