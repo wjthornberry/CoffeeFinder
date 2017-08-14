6b08337ab06cc6ac79bf991118f01687ce17f9a8
Jake Thornberry <wjthornberry@gmail.com>
2017-08-14 14:50:48 -0500

    Testing line 26 for static files

--- server.js
+++ server.js
@@ -1,17 +1,12 @@
 // Dependencies - npm packages to give the server funcitonality
-var express = require('express'),
-    bodyParser = require('body-parser'),
-    path = require('path');
-
-// Express configuration to set up the basic properties of the server
-var app = express();
-
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
 // Sets the server port.
 var PORT = process.env.PORT || 3000;
 
-app.listen(PORT, function () {
-    console.log(`Listening on Port ${PORT}`)
-});
+// Express configuration to set up the basic properties of the server
+var app = express();
 
 // Sets up bodyParser, which allows the server to interpret the data sent to it
 // Boilerplate code
@@ -20,14 +15,21 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-app.use('/static', express.static(path.join(__dirname, 'app/public')))
-
 // Router points server to the route files
 // These are the server's map for how to respond to a user when they 
 // visit or request data from the URLs.
 
-require('./app/routing/htmlroutes.js')(app);
-require('./app/routing/apiroutes.js')(app);
+require('./app/routing/htmlRoutes.js')(app);
+require('./app/routing/apiRoutes.js')(app);
+
+// // Loads static files
+app.use(express.static('./app/public'));
+// app.use('/static', express.static(path.join(__dirname, 'app/public')))
+
+app.listen(PORT, function () {
+    console.log(`Listening on Port ${PORT}`)
+});
+
 
 // Heroku link
 // https://nameless-dusk-37653.herokuapp.com/
