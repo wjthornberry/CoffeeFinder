5fbe75fd494337981e4eb87447cfbeb99d4f5071
Jake Thornberry <wjthornberry@gmail.com>
2017-08-14 17:25:06 -0500

    Take comments out

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friends = [
     {
         name: 'Aslan',
-        photo: '../public/images/aslan.png',
+        photo: '.././public/images/aslan.png',
         scores: [
             '5',
             '5',
@@ -15,166 +15,166 @@ var friends = [
             '2'
         ]
     },
-    // {
-    //     name: 'Kathryn',
-    //     photo: '../public/images/kathryn.png',
-    //     scores: [
-    //         '5',
-    //         '4',
-    //         '5',
-    //         '1',
-    //         '5',
-    //         '5',
-    //         '5',
-    //         '5',
-    //         '5',
-    //         '5'
-    //     ]
-    // },
-    // {
-    //     name: 'Jake',
-    //     photo: '../public/images/jake.png',
-    //     scores: [
-    //         '5',
-    //         '5',
-    //         '5',
-    //         '5',
-    //         '5',
-    //         '5',
-    //         '5',
-    //         '5',
-    //         '5',
-    //         '5'
-    //     ]
-    // },
-    // {
-    //     name: 'GOB',
-    //     photo: '../public/images/gob.png',
-    //     score: [
-    //         '1',
-    //         '2',
-    //         '2',
-    //         '1',
-    //         '3',
-    //         '4',
-    //         '5',
-    //         '2',
-    //         '1',
-    //         '2'
-    //     ]
-    // },
-    // {
-    //     name: 'Donald Trump',
-    //     photo: '../public/images/thedonald.png',
-    //     score: [
-    //         '1',
-    //         '4',
-    //         '1',
-    //         '1',
-    //         '1',
-    //         '2',
-    //         '5',
-    //         '1',
-    //         '1',
-    //         '4'
-    //     ]
-    // },
-    // {
-    //     name: 'Barack Obama',
-    //     photo: '../public/images/barack.png',
-    //     score: [
-    //         '5',
-    //         '4',
-    //         '4',
-    //         '1',
-    //         '3',
-    //         '5',
-    //         '3',
-    //         '3',
-    //         '4',
-    //         '3',
-    //     ]
-    // },
-    // {
-    //     name: 'Harriet',
-    //     photo: '../public/images/harriet.png',
-    //     score: [
-    //         '4',
-    //         '3',
-    //         '5',
-    //         '5',
-    //         '5',
-    //         '3',
-    //         '4',
-    //         '1',
-    //         '4',
-    //         '2'
-    //     ]
-    // },
-    // {
-    //     name: 'Paul McCartney',
-    //     photo: '../public/images/paul.png',
-    //     score: [
-    //         '4',
-    //         '5',
-    //         '3',
-    //         '2',
-    //         '3',
-    //         '4',
-    //         '4',
-    //         '5',
-    //         '3',
-    //         '3'
-    //     ]
-    // },
-    // {
-    //    name: 'Ruth Bader Ginsburg',
-    //    photo: '../public/images/ruth.png',
-    //    score: [
-    //        '4',
-    //        '3',
-    //        '4',
-    //        '1',
-    //        '2',
-    //        '5',
-    //        '2',
-    //        '2',
-    //        '3',
-    //        '3'
-    //    ] 
-    // },
-    // {
-    //     name: 'Calvin',
-    //     photo: '../public/images/calvin.png',
-    //     score: [
-    //         '5',
-    //         '4',
-    //         '5',
-    //         '2',
-    //         '1',
-    //         '3',
-    //         '3',
-    //         '5',
-    //         '1',
-    //         '5'
-    //     ]
-    // },
-    // {
-    //     name: 'Hobbes',
-    //     photo: '../public/images/hobbes.png',
-    //     score: [
-    //         '5',
-    //         '2',
-    //         '5',
-    //         '2',
-    //         '4',
-    //         '4',
-    //         '5',
-    //         '4',
-    //         '2',
-    //         '5'
-    //     ]
-    // }
+    {
+        name: 'Kathryn',
+        photo: '../public/images/kathryn.png',
+        scores: [
+            '5',
+            '4',
+            '5',
+            '1',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5'
+        ]
+    },
+    {
+        name: 'Jake',
+        photo: '../public/images/jake.png',
+        scores: [
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5'
+        ]
+    },
+    {
+        name: 'GOB',
+        photo: '../public/images/gob.png',
+        score: [
+            '1',
+            '2',
+            '2',
+            '1',
+            '3',
+            '4',
+            '5',
+            '2',
+            '1',
+            '2'
+        ]
+    },
+    {
+        name: 'Donald Trump',
+        photo: '../public/images/thedonald.png',
+        score: [
+            '1',
+            '4',
+            '1',
+            '1',
+            '1',
+            '2',
+            '5',
+            '1',
+            '1',
+            '4'
+        ]
+    },
+    {
+        name: 'Barack Obama',
+        photo: '../public/images/barack.png',
+        score: [
+            '5',
+            '4',
+            '4',
+            '1',
+            '3',
+            '5',
+            '3',
+            '3',
+            '4',
+            '3',
+        ]
+    },
+    {
+        name: 'Harriet',
+        photo: '../public/images/harriet.png',
+        score: [
+            '4',
+            '3',
+            '5',
+            '5',
+            '5',
+            '3',
+            '4',
+            '1',
+            '4',
+            '2'
+        ]
+    },
+    {
+        name: 'Paul McCartney',
+        photo: '../public/images/paul.png',
+        score: [
+            '4',
+            '5',
+            '3',
+            '2',
+            '3',
+            '4',
+            '4',
+            '5',
+            '3',
+            '3'
+        ]
+    },
+    {
+       name: 'Ruth Bader Ginsburg',
+       photo: '../public/images/ruth.png',
+       score: [
+           '4',
+           '3',
+           '4',
+           '1',
+           '2',
+           '5',
+           '2',
+           '2',
+           '3',
+           '3'
+       ] 
+    },
+    {
+        name: 'Calvin',
+        photo: '../public/images/calvin.png',
+        score: [
+            '5',
+            '4',
+            '5',
+            '2',
+            '1',
+            '3',
+            '3',
+            '5',
+            '1',
+            '5'
+        ]
+    },
+    {
+        name: 'Hobbes',
+        photo: '../public/images/hobbes.png',
+        score: [
+            '5',
+            '2',
+            '5',
+            '2',
+            '4',
+            '4',
+            '5',
+            '4',
+            '2',
+            '5'
+        ]
+    }
 ];
 
 // Exports the friends array. This allows other files to access it via require
